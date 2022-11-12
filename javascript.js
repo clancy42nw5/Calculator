44e2ed7162f7b2d8c12c99ca3ec683ccf9495f87
Clancy Wilson <116132142+clancy42nw5@users.noreply.github.com>
2022-11-12 22:01:09 +1100

    Add function to number keys

--- javascript.js
+++ javascript.js
@@ -1,9 +1,51 @@
-const integer = document.querySelector('.integer');
-const operators = document.querySelector('.operators');
-const baseFunc = document.querySelector('.baseFunc');
 const display = document.querySelector('.display');
 
-const newDisplay = function(x) {
+let userInput = 0
+let bankedNumber = 0
+
+const intButtons = document.querySelectorAll('.integer');
+intButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        x = button.textContent;
+        if (userInput == 0) {
+            userInput = x;
+            changeDisplay(userInput);
+        } else {
+            userInput = userInput * 1;
+            console.log(userInput)
+            userInput += x;
+            changeDisplay(userInput);
+        } 
+    });
+});
+
+/*
+const funcButtons = document.querySelectorAll('.operators');
+funcButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+    })
+})
+*/
+
+const percentButton = document.querySelectorAll('#percent');
+percentButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        userInput = userInput / 100;
+        changeDisplay(userInput);
+    })
+})
+
+const clearAllButton = document.querySelectorAll('#AC');
+clearAllButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        userInput = 0
+        bankedNumber = 0
+        changeDisplay(userInput);
+    })
+})
+
+const changeDisplay = function(x) {
     display.textContent = x;
 }
 
@@ -27,16 +69,8 @@ const divide = function(x, y) {
     return z;
 }
 
-const clear = function() {
-    let z = 0;
-    return z;
-}
-
-userInput = 0
-
 /*
-
-userInput = (x * 10 + y) = x
+userInput = (x * 1 + y) = x
 bankedNumber = 0
 operator()
 
@@ -52,5 +86,4 @@ Whe equals is pressed, the function() uses that operator on the last index
 in the array, returning the new number to be pushed to the array.
 
 If a FLOAT is returned, it should be rounded to the nearest 10th degree.
-
 */
